0d0e9846b60dd0180e1cd6ff11bc29920873d838
nyx <93729943+nyxx1@users.noreply.github.com>
2022-06-19 08:02:44 +0530

    Add files via upload

---
+++ bubblesort.py
@@ -0,0 +1,15 @@
+# Algorithm (ascending order)
+# From index 0, compare the first element to to the adjacent element in the list
+# if the current element is greater than the next element, swap.
+# if the current element is less than the next element, move to the next element, repeat step 1
+
+def bubblesort(list1):
+	for j in range(len(list1)-1):
+		for x in range(len(list1)-1):
+			if list1[x] > list1[x+1]:
+				list1[x],list1[x+1] = list1[x+1],list1[x]
+			print(list1)
+
+list1=list(map(int,input().strip().split()))
+bubblesort(list1)
+
