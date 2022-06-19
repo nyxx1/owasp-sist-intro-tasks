d30fa8518eece32c84750942c882479248f87d44
nyx <93729943+nyxx1@users.noreply.github.com>
2022-06-19 08:01:25 +0530

    Add files via upload

---
+++ 220A.py
@@ -0,0 +1,13 @@
+n=int,input()
+a = list(map(int,input().strip().split()))
+b = sorted(a)
+
+swapcount = 0
+for x in range(len(a)):
+	if a[x] != b[x]:
+		swapcount+=1
+
+if swapcount==0 or swapcount==2:
+	print("YES")
+else:
+	print("NO")
